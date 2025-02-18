893446b0c9dd6a69187f79f50052e3ebcd8e2ca9
πa <76558220+rkdud007@users.noreply.github.com>
2025-02-18 19:11:12 +0900

    bump(adapter): hyper version 1 (#120)
    update `hyper` to version 1
    
    hive is same : finished suites=1 tests=226 failed=15

--- bin/adapter/src/main.rs
+++ bin/adapter/src/main.rs
@@ -1,13 +1,16 @@
 use alloy_rpc_types_engine::{PayloadId, PayloadStatus};
-use futures_util::TryFutureExt;
-use hyper::{
-    service::{make_service_fn, service_fn},
-    Body, Client, Request, Response, Server,
+use bytes::Bytes;
+use http_body_util::{combinators::BoxBody, BodyExt, Full};
+use hyper::{body::Incoming, server::conn::http1, service::service_fn, Request, Response};
+use hyper_rustls::HttpsConnectorBuilder;
+use hyper_util::{
+    client::legacy::Client,
+    rt::{TokioExecutor, TokioIo},
 };
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
-use std::convert::Infallible;
-use tokio::io::{self};
+use std::{collections::HashMap, net::SocketAddr};
+use tokio::{self, net::TcpListener};
 use tracing::{error, info, Level};
 
 const RETH_AUTH: &str = "http://127.0.0.1:8651";
@@ -25,131 +28,127 @@ struct RethPayloadResponse {
 }
 
 async fn forward_request(
-    req: Request<Body>,
+    req: Request<Incoming>,
     is_auth_server: bool,
-) -> Result<Response<Body>, hyper::Error> {
-    let (parts, body) = req.into_parts();
-    let whole_body = hyper::body::to_bytes(body).await?;
-    let body_str = String::from_utf8_lossy(&whole_body);
+) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
+    let reth_uri = if is_auth_server { RETH_AUTH } else { RETH_HTTP };
+    let req_method = req.method().clone();
+    let req_headers = req.headers().clone();
+    let whole_body = req.collect().await?.to_bytes();
+    let params =
+        form_urlencoded::parse(&whole_body).into_owned().collect::<HashMap<String, String>>();
 
-    // todo: handle it as enum
-    let mut is_engine_method = false;
-    if is_auth_server {
-        if let Ok(json_body) = serde_json::from_str::<Value>(&body_str) {
-            if let Some(method_value) = json_body.get("method") {
-                let method_str = method_value.as_str().unwrap_or_default();
-                // Mark `is_engine_method` as true if:
-                //  1) method starts with "engine"
-                //  2) method does NOT start with "engine_get"
-                if method_str.starts_with("engine") && !method_str.starts_with("engine_get") {
-                    is_engine_method = true;
-                }
-            }
-        }
-    }
+    // `is_engine_method` as true if:
+    //  1) method starts with "engine"
+    //  2) method does NOT start with "engine_get"
+    let is_engine_method = params
+        .get("method")
+        .map(|method| method.starts_with("engine") && !method.starts_with("engine_get"))
+        .unwrap_or(false);
 
-    let client = Client::new();
+    let https =
+        HttpsConnectorBuilder::new().with_webpki_roots().https_or_http().enable_http1().build();
+    let client = Client::builder(TokioExecutor::new()).build(https);
     let build_request = |uri: &str| {
-        let mut builder = Request::builder().method(parts.method.clone()).uri(uri);
-        for (key, value) in parts.headers.iter() {
+        let mut builder = Request::builder().method(req_method.clone()).uri(uri);
+        for (key, value) in req_headers.iter() {
             builder = builder.header(key, value);
         }
-        builder.body(Body::from(whole_body.clone()))
+        builder.body(Full::new(whole_body.clone()).boxed()).unwrap()
     };
 
-    // Send request to Reth and await its response
-    let reth = if is_auth_server { RETH_AUTH } else { RETH_HTTP };
-    let reth_req = build_request(reth).unwrap();
-    let reth_resp = client.request(reth_req).await?;
-    info!("Received from Reth, not payload: {:?}", reth_resp);
+    info!(target: "adapter", "Sending request to reth");
+    let reth_req = build_request(reth_uri);
+    let reth_res = client.request(reth_req).await.unwrap();
+    let (parts, body) = reth_res.into_parts();
+    // if it's not engine method, return reth response
+    if !is_engine_method {
+        let boxed_body = BoxBody::new(body);
+        info!(target: "adapter", "Sending response from reth");
+        return Ok(Response::from_parts(parts, boxed_body));
+    }
 
-    // If it's an engine method, send request to Ress and await its response
-    if is_engine_method {
-        let (reth_parts, reth_body) = reth_resp.into_parts();
-        let reth_body_bytes = hyper::body::to_bytes(reth_body).await?;
-        let top_level: Value = serde_json::from_slice(&reth_body_bytes).unwrap();
-        match top_level.get("result") {
-            Some(result_value) => {
-                let is_payload_id =
-                    match serde_json::from_value::<RethPayloadResponse>(result_value.clone()) {
-                        Ok(json_value) => json_value.payload_id,
-                        Err(_) => None,
-                    };
+    let reth_body_bytes = body.collect().await?.to_bytes();
+    info!(target: "adapter", "Sending request to ress");
+    let ress_req = build_request(RESS_AUTH);
+    let ress_res = client.request(ress_req).await.unwrap();
+    let (mut ress_parts, ress_body) = ress_res.into_parts();
+    let ress_body_bytes = ress_body.collect().await?.to_bytes();
 
-                let ress_req = build_request(RESS_AUTH).unwrap();
-                info!("Sending request to Ress: {:?}", ress_req);
-
-                let ress_resp = client.request(ress_req).await?;
-                if let Some(reth_payload_id) = is_payload_id {
-                    let (mut ress_parts, ress_body) = ress_resp.into_parts();
-                    info!("reth payload id: {:?}", reth_payload_id);
-                    let ress_body_bytes = hyper::body::to_bytes(ress_body).await?;
-                    let mut top_level: Value = serde_json::from_slice(&ress_body_bytes).unwrap();
-                    if let Some(result_obj) = top_level.get_mut("result") {
-                        if let Some(result_map) = result_obj.as_object_mut() {
-                            result_map.insert(
-                                "payloadId".to_string(),
-                                serde_json::Value::String(reth_payload_id.to_string()),
-                            );
-                        }
+    // Process payload ID replacement
+    let top_level: Value = serde_json::from_slice(&reth_body_bytes).unwrap();
+    if let Some(result_value) = top_level.get("result") {
+        if let Ok(reth_response) =
+            serde_json::from_value::<RethPayloadResponse>(result_value.clone())
+        {
+            if let Some(reth_payload_id) = reth_response.payload_id {
+                let mut ress_top_level: Value = serde_json::from_slice(&ress_body_bytes).unwrap();
+                if let Some(result_obj) = ress_top_level.get_mut("result") {
+                    if let Some(result_map) = result_obj.as_object_mut() {
+                        result_map.insert(
+                            "payloadId".to_string(),
+                            serde_json::Value::String(reth_payload_id.to_string()),
+                        );
                     }
-
-                    let new_body_bytes = serde_json::to_vec(&top_level).unwrap();
-
-                    // Remove or recalc content-length
-                    ress_parts.headers.remove("content-length");
-                    ress_parts.headers.insert(
-                        hyper::header::CONTENT_LENGTH,
-                        hyper::header::HeaderValue::from_str(&new_body_bytes.len().to_string())
-                            .unwrap(),
-                    );
-                    let new_response = Response::from_parts(ress_parts, Body::from(new_body_bytes));
-                    Ok(new_response)
-                } else {
-                    info!("Received from Ress, not payload: {:?}", ress_resp);
-                    Ok(ress_resp)
                 }
-            }
-            None => {
-                info!("rpc response status is: {:?}", reth_parts.status);
-                let ress_req = build_request(RESS_AUTH).unwrap();
-                info!("Sending request to Ress: {:?}", ress_req);
-                let ress_resp = client.request(ress_req).await?;
-                Ok(ress_resp)
+                let new_body = serde_json::to_vec(&ress_top_level).unwrap();
+                ress_parts.headers.remove(hyper::header::CONTENT_LENGTH);
+                ress_parts.headers.insert(
+                    hyper::header::CONTENT_LENGTH,
+                    new_body.len().to_string().parse().unwrap(),
+                );
+                info!(target: "adapter", "Sending response from ress");
+                return Ok(Response::from_parts(ress_parts, full(new_body)));
             }
         }
-    } else {
-        Ok(reth_resp)
     }
+    info!(target: "adapter", "Sending response from ress");
+    Ok(Response::from_parts(ress_parts, full(ress_body_bytes)))
+}
+
+pub(crate) fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
+    Full::new(chunk.into()).map_err(|never| match never {}).boxed()
 }
 
 #[tokio::main]
-async fn main() -> io::Result<()> {
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     tracing_subscriber::fmt().with_max_level(Level::INFO).init();
 
-    let auth = ([0, 0, 0, 0], 8551).into();
-    let http = ([0, 0, 0, 0], 8545).into();
+    let auth_addr: SocketAddr = ([0, 0, 0, 0], 8551).into();
+    let http_addr: SocketAddr = ([0, 0, 0, 0], 8545).into();
 
-    let auth_server = Server::bind(&auth).serve(make_service_fn(|_conn| async {
-        Ok::<_, Infallible>(service_fn(|req| forward_request(req, true)))
-    }));
+    let auth_listener = TcpListener::bind(auth_addr).await?;
+    let http_listener = TcpListener::bind(http_addr).await?;
+    info!("Listening on http://{} and http://{}", auth_addr, http_addr);
 
-    let http_server = Server::bind(&http).serve(make_service_fn(|_conn| async {
-        Ok::<_, Infallible>(service_fn(|req| forward_request(req, false)))
-    }));
-
-    info!("Listening on http://{} and http://{}", auth, http);
-
-    tokio::try_join!(
-        auth_server.map_err(|e| {
-            error!("Auth server error: {}", e);
-            io::Error::new(io::ErrorKind::Other, e)
-        }),
-        http_server.map_err(|e| {
-            error!("Http error: {}", e);
-            io::Error::new(io::ErrorKind::Other, e)
-        })
-    )?;
-
+    let auth_task = tokio::spawn(async move {
+        loop {
+            let (auth_tcp, _) = auth_listener.accept().await.unwrap();
+            let auth_io = TokioIo::new(auth_tcp);
+            tokio::spawn(async move {
+                if let Err(err) = http1::Builder::new()
+                    .serve_connection(auth_io, service_fn(|req| forward_request(req, true)))
+                    .await
+                {
+                    error!("Error serving auth connection: {:?}", err);
+                }
+            });
+        }
+    });
+    let http_task = tokio::spawn(async move {
+        loop {
+            let (http_tcp, _) = http_listener.accept().await.unwrap();
+            let http_io = TokioIo::new(http_tcp);
+            tokio::spawn(async move {
+                if let Err(err) = http1::Builder::new()
+                    .serve_connection(http_io, service_fn(|req| forward_request(req, false)))
+                    .await
+                {
+                    error!("Error serving http connection: {:?}", err);
+                }
+            });
+        }
+    });
+    tokio::try_join!(auth_task, http_task)?;
     Ok(())
 }
